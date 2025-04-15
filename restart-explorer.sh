11cb889cce6d10de62f0adce9e247b22d09c3e05
rachit-j <rjaiswal.sd.77@gmail.com>
2025-04-14 23:04:06 -0700

    added no cache for better build

--- restart-explorer.sh
+++ restart-explorer.sh
@@ -5,5 +5,5 @@ cd /home/ubuntu/containers/explorer/urban-explorer/
 
 # Rebuild and restart the container
 /usr/bin/docker-compose down
-/usr/bin/docker-compose build
+/usr/bin/docker-compose build --no-cache
 /usr/bin/docker-compose up -d
